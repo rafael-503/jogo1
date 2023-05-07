7ec3f06f69dfe792a0b265a26bbed53c8712eb69
ArielWC41 <132675583+ArielWC41@users.noreply.github.com>
2023-05-07 10:41:56 -0300

    Add files via upload

---
+++ The Invasion/Entidades/Personagens/Personagem.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../../stdafx.h"
+#include <SFML/Graphics.hpp>
+#include "../Entidade.h"
+
+
+namespace Entidades{
+
+    namespace Personagens{
+
+        class Personagem: public Entidade{
+        protected:
+
+        public:
+            Personagem(sf::Vector2f tam_corpo);
+            ~Personagem();
+            virtual void executar() = 0;
+
+        };
+    }
+}
