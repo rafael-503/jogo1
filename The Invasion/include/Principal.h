783f3b266f85e1d58b3844e998ccee131134d125
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-08 13:15:53 -0300

    revert

--- The Invasion/include/Principal.h
+++ The Invasion/include/Principal.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "stdafx.h"
-#include "Entidades/Personagens/Jogador.h"
-#include "Entidades/Personagens/Inimigo.h"
-#include "Entidades/Obstaculos/Obstaculo.h"
+#include "include/Entidades/Personagens/Jogador.h"
+#include "include/Entidades/Personagens/Inimigo.h"
+#include "include/Entidades/Obstaculos/Obstaculo.h"
 #include <SFML/Graphics.hpp>
 
 class Principal {
