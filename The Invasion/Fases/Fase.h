f1be3abe7c40ecaed4671d3de3b248c68ccfab33
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-19 19:04:48 -0300

    adicionado classe Fase

---
+++ The Invasion/Fases/Fase.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../Ente.h"
+#include "../Listas/ListaEntidades.h"
+#include "../Gerenciadores/Gerenciador_Colisoes.h"
+
+namespace Fases {
+	class Fase : public Ente {
+		protected:
+			Gerenciadores::Gerenciador_Colisoes* pColisao;
+			Listas::ListaEntidades listaPersonagens;
+			Listas::ListaEntidades listaObstaculos;
+
+		public:
+			Fase();
+			~Fase();
+			void gerenciarColisoes();
+			virtual void executar();
+			virtual void criarInimigos() = 0;
+			virtual void criarObstaculos() = 0;
+	};
+}
+
+
+
