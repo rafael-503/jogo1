fb2d2bdfdefeb9d44b6f4d09770d9d6f2310e380
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-24 00:47:25 -0300

    adicionado Fase1 ao Principal.cpp

--- The Invasion/Principal.cpp
+++ The Invasion/Principal.cpp
@@ -8,7 +8,7 @@
 #include "Entidades/Personagens/Cachorro.h"
 #include "Entidades/Personagens/Lenhador.h"
 #include "Entidades/Personagens/Soldado.h"
-
+#include "Fases/Fase1.h"
 
 using namespace Entidades;
 using namespace Personagens;
@@ -29,89 +29,56 @@ Principal::Principal():
 Principal::~Principal(){}
 
 void Principal::executar(){
-
+    /*
     Soldado inimigo(sf::Vector2f(80.0f, 80.0f), sf::Vector2f(80.0f, 80.0f));
     Jogador jogador(sf::Vector2f(80.0f, 80.0f));
     inimigo.setJogador(&jogador);
 
     Entidade* pEntidade = NULL;
-    Listas::ListaEntidades listaEntidades;
-
     Obstaculo* pObstaculo = NULL;
+    Listas::ListaEntidades listaPersonagens;
+    Listas::ListaEntidades listaObstaculos;
 
     Jogador* pJogador = &jogador;
     Inimigo* pInimigo = &inimigo;
 
-
     pGerencia_Colisoes->incluiInimigo(pInimigo);
     pEntidade = static_cast<Entidade*> (pJogador);
-    listaEntidades.inserir(pEntidade);
+    listaPersonagens.inserir(pEntidade);
     pEntidade = static_cast<Entidade*> (pInimigo);
-    listaEntidades.inserir(pEntidade);
+    listaPersonagens.inserir(pEntidade);
 
 
     pObstaculo = new Plataforma(sf::Vector2f(200.0f, 50.0f), sf::Vector2f(100.0f, 700.0f));
     pGerencia_Colisoes->incluiObstaculo(pObstaculo);
     pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
+    listaObstaculos.inserir(pEntidade);
 
-    pObstaculo = new Espinhos(sf::Vector2f(50.0f, 50.0f), sf::Vector2f(700.0f, 400.0f));
-    pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
-
-    pObstaculo = new Caixa(sf::Vector2f(50.0f, 50.0f), sf::Vector2f(900.0f, 450.0f));
-    pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
-
-    pObstaculo = new Plataforma(sf::Vector2f(180.0f, 50.0f), sf::Vector2f(300.0f, 600.0f));
-    pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
-
-    pObstaculo = new Plataforma(sf::Vector2f(180.0f, 50.0f), sf::Vector2f(500.0f, 500.0f));
-    pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
-    //
-    //pObstaculo = new Plataforma(sf::Vector2f(50.0f, 50.0f), sf::Vector2f(700.0f, 400.0f));
-    //pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    //pEntidade = static_cast<Entidade*> (pObstaculo);
-    //listaEntidades.inserir(pEntidade);
-    //
-    pObstaculo = new Plataforma(sf::Vector2f(180.0f, 50.0f), sf::Vector2f(300.0f, 350.0f));
-    pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
-
-    pObstaculo = new Plataforma(sf::Vector2f(180.0f, 50.0f), sf::Vector2f(800.0f, 600.0f));
-    pGerencia_Colisoes->incluiObstaculo(pObstaculo);
-    pEntidade = static_cast<Entidade*> (pObstaculo);
-    listaEntidades.inserir(pEntidade);
     pGerenciaEventos->setJogador(pJogador);
+    */
+    Fases::Fase1 fase1;
+    fase1.criarObstaculos();
+    fase1.criarPersonagens();
 
     while (pGrafico->verificarJanelaAberta())
     {
-        /*
-        sf::Event event;
-
-        if(pGrafico->getWindow()->pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                pGrafico->fecharJanela();
-        }
-        */
         pGerenciaEventos->verificarEventos();
         pGrafico->limparJanela();
-        //pGrafico->getWindow()->draw(sprite); // fundo
 
-        listaEntidades.executar();
-        pGerencia_Colisoes->testa_colisoes(&jogador);
-        listaEntidades.seDesenhe();
+        fase1.executar();
+        //listaPersonagens.executar();
+        //listaObstaculos.executar();
+        //pGerencia_Colisoes->testa_colisoes(&jogador);
+
+        //listaPersonagens.seDesenhe();
+        //listaObstaculos.seDesenhe();
+
 
         sf::Time tempo_por_frame = sf::seconds(1.0f / 60.0f); // limita a atualização a 60 fps
         sf::sleep(tempo_por_frame);
         pGrafico->mostrarElementos();
     }
-    listaEntidades.esvaziar();
+    fase1.esvaziar();
+    //listaPersonagens.esvaziar();
+    //listaObstaculos.esvaziar();
 }
