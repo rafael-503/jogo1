73c0b7ebf5f57820eeaa1b4d10eff51f4fe70a10
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-24 00:33:59 -0300

    modificado classe Fase1 e modificado ListaEntidades.h

--- The Invasion/Fases/Fase1.cpp
+++ The Invasion/Fases/Fase1.cpp
@@ -8,13 +8,13 @@ Fase1::Fase1() {
 }
 
 Fase1::~Fase1() {
-
+ 
 }
 
 void Fase1::atualizar() {
 	listaPersonagens.executar();
 	listaObstaculos.executar();
-
+    //pColisao->testa_colisoes(&jogador);
 	listaPersonagens.seDesenhe();
 	listaObstaculos.seDesenhe();
 }
@@ -25,9 +25,32 @@ void Fase1::esvaziar() {
 }
 
 void Fase1::criarPersonagens() {
+    Entidades::Personagens::Soldado inimigo(sf::Vector2f(80.0f, 80.0f), sf::Vector2f(80.0f, 80.0f));
+    Entidades::Personagens::Jogador jogador(sf::Vector2f(80.0f, 80.0f));
+    inimigo.setJogador(&jogador);
 
+    Entidade* pEntidade = NULL;
+
+    Entidades::Personagens::Jogador* pJogador = &jogador;
+    Entidades::Personagens::Inimigo* pInimigo = &inimigo;
+
+    pColisao->incluiInimigo(pInimigo);
+    pEntidade = static_cast<Entidade*> (pJogador);
+    listaPersonagens.inserir(pEntidade);
+    pEntidade = static_cast<Entidade*> (pInimigo);
+    listaPersonagens.inserir(pEntidade);
+
+    pEventos->setJogador(pJogador);
+    //
+    pColisao->testa_colisoes(&jogador);
 }
 
 void Fase1::criarObstaculos() {
+    Entidade* pEntidade = NULL;
+    Entidades::Obstaculos::Obstaculo* pObstaculo = NULL;
 
+    pObstaculo = new Entidades::Obstaculos::Plataforma(sf::Vector2f(200.0f, 50.0f), sf::Vector2f(100.0f, 700.0f));
+    pColisao->incluiObstaculo(pObstaculo);
+    pEntidade = static_cast<Entidade*> (pObstaculo);
+    listaObstaculos.inserir(pEntidade);
 }
