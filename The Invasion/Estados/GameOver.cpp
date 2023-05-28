11926460cbaa8b1dfc25ebb7c79058c8640ec1ba
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-28 18:59:00 -0300

    modificado posicao dos botoes e funcionalidades ao clicar

--- The Invasion/Estados/GameOver.cpp
+++ The Invasion/Estados/GameOver.cpp
@@ -1,16 +1,17 @@
 #include "GameOver.h"
+#include "../Gerenciadores/GerenciadorEstado.h"
 using namespace Estados;
 
-GameOver::GameOver() :BotaoTentarNovamente("Tentar novamente", font), BotaoMenuPrincipal("Menu Principal", font) {
+GameOver::GameOver() :Estado(), BotaoTentarNovamente("Tentar novamente", font), BotaoMenuPrincipal("Menu Principal", font) {
     sf::Vector2u tamJanela(600.f, 400.f);
     BotaoTentarNovamente.setScale(1.5f, 1.5f);
     BotaoMenuPrincipal.setScale(1.5f, 1.5f);
 
     sf::FloatRect tamBotaoTentarNovamente = BotaoTentarNovamente.getLocalBounds();
-    BotaoTentarNovamente.setPosition(-(tamJanela.x / 2 - tamBotaoTentarNovamente.width + 600), 550);
+    BotaoTentarNovamente.setPosition(-(tamJanela.x / 2 - tamBotaoTentarNovamente.width - 120), 550);
 
     sf::FloatRect tamBotaoMenuPrincipal = BotaoMenuPrincipal.getLocalBounds();
-    BotaoMenuPrincipal.setPosition(tamJanela.x / 2 - 250, 550);
+    BotaoMenuPrincipal.setPosition(tamJanela.x / 2 + 500, 550);
 }
 
 GameOver::~GameOver() {
@@ -19,9 +20,36 @@ GameOver::~GameOver() {
 
 void GameOver::PrimeiroExecutar() {
 	pGrafico->carregarFundo("The invasion/assets/fundo/fundo3.png");
+    pGrafico->atualizarView(sf::Vector2f(600.0f, 400.0f));
 }
 
 void GameOver::executar() {
+    sf::Vector2i posMouse = sf::Mouse::getPosition(*pGrafico->getWindow());
+    sf::Vector2f fPosMouse((float)posMouse.x, (float)posMouse.y);
+
+    sf::FloatRect rectTentarNovamente(BotaoTentarNovamente.getGlobalBounds()), rectMenuPrincipal(BotaoMenuPrincipal.getGlobalBounds()),
+        corpoMouse(fPosMouse, sf::Vector2f(5.0f, 5.0f));
+
+    if (rectTentarNovamente.intersects(corpoMouse)) {
+        BotaoTentarNovamente.setFillColor(sf::Color::Red);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            //Gerenciadores::GerenciadorEstado::pGEstados->setEstadoAtual("NovoJogo");
+            cout << "TENTAR NOVAMENTE" << endl;
+        }
+    }
+    else {
+        BotaoTentarNovamente.setFillColor(sf::Color::White);
+    }
+
+    if (rectMenuPrincipal.intersects(corpoMouse)) {
+        BotaoMenuPrincipal.setFillColor(sf::Color::Red);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            Gerenciadores::GerenciadorEstado::pGEstados->setEstadoAtual("MenuPrincipal");
+    }
+    else {
+        BotaoMenuPrincipal.setFillColor(sf::Color::White);
+    }
+
 	pGrafico->desenharElemento(BotaoTentarNovamente);
 	pGrafico->desenharElemento(BotaoMenuPrincipal);
 }
