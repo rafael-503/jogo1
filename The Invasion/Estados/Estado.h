92ce2c54f30f75ac491555beca6b2b127c5991b1
ArielWC41 <132675583+ArielWC41@users.noreply.github.com>
2023-05-28 22:20:05 -0300

    Adicionado Bool executando á classe Estado

--- The Invasion/Estados/Estado.h
+++ The Invasion/Estados/Estado.h
@@ -8,10 +8,12 @@ namespace Estados{
         protected:
             sf::Font font;
             Gerenciadores::GerenciadorGrafico* pGrafico;
+            bool executando;
 
         public:
             Estado();
             virtual ~Estado();
+            void setExecutando(bool ativo);
             virtual void TeclaPressionada(const sf::Keyboard::Key tecla) = 0;
             virtual void PrimeiroExecutar() = 0;
             virtual void executar() = 0;
