b26c6f8fd04935824d010913b53573bba310fcce
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-30 11:46:14 -0300

    modificado movimento aleatorio do inimigo

--- The Invasion/Entidades/Personagens/Inimigo.cpp
+++ The Invasion/Entidades/Personagens/Inimigo.cpp
@@ -41,15 +41,15 @@ void Inimigo::persegueJogador(sf::Vector2f posJog, sf::Vector2f posInim) {
 
 void Inimigo::moveAleatorio() {
     if (move_aleatorio == 0) // direita
-        corpo.move(vel.x, 0.0f);
-    else if (move_aleatorio == 1) //esquerda
-        corpo.move(-vel.x, 0.0f);
+        corpo.move(vel.x/2, 0.0f);
+    else if (move_aleatorio == 1 || move_aleatorio == 4) //esquerda
+        corpo.move(-vel.x/2, 0.0f);
     else if(move_aleatorio == 2) //parado
         corpo.move(0.0f, 0.0f);
 
     float dt = relogio.getElapsedTime().asSeconds();
     if (dt > 1.0f) {
-        move_aleatorio = rand() % 3;
+        move_aleatorio = rand() % 4;
         relogio.restart();
     }
 }
