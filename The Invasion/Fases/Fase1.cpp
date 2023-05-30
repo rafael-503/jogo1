a95edcfad992d4ba6da8366d901fde949b85d6e4
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-30 14:01:03 -0300

    modificado construtorObstaculos em fase1

--- The Invasion/Fases/Fase1.cpp
+++ The Invasion/Fases/Fase1.cpp
@@ -36,16 +36,16 @@ void Fase1::esvaziar() {
 	listaObstaculos.esvaziar();
 }
 
-void Fase1::construtorObstaculos(const std::string& tipo, const sf::Vector2f& tam, const sf::Vector2f& pos) {
+void Fase1::construtorObstaculos(const std::string& tipo, const sf::Vector2f& pos, sf::Vector2f tam) {
     Entidades::Obstaculos::Obstaculo* pObstaculo = NULL;
     Entidade* pEntidade = NULL;
 
     if (tipo == "Caixa")
-        pObstaculo = new Entidades::Obstaculos::Caixa(tam, pos);
+        pObstaculo = new Entidades::Obstaculos::Caixa(pos);
     else if (tipo == "Espinhos")
-        pObstaculo = new Entidades::Obstaculos::Espinhos(tam, pos);
+        pObstaculo = new Entidades::Obstaculos::Espinhos(pos);
     else if (tipo == "Plataforma")
-        pObstaculo = new Entidades::Obstaculos::Plataforma(tam, pos);
+        pObstaculo = new Entidades::Obstaculos::Plataforma(pos, tam);
     else {
         cout << "Erro: Tipo de obstaculo invalido" << endl;
         return;
@@ -116,30 +116,31 @@ void Fase1::criarPersonagens() {
 }
 
 void Fase1::criarObstaculos() {
-    construtorObstaculos("Caixa", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(330.0f, 0.0f));
+    construtorObstaculos("Caixa", sf::Vector2f(330.0f, 0.0f));
     construtorObstaculos("Plataforma", sf::Vector2f(25000.0f, 50.0f), sf::Vector2f(0.0f, 800.0f)); // remover
 
-    construtorObstaculos("Plataforma", sf::Vector2f(250.0f, 50.0f), sf::Vector2f(0.0f, 750.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(180.0f, 50.0f), sf::Vector2f(300.0f, 600.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(180.0f, 50.0f), sf::Vector2f(500.0f, 500.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(180.0f, 50.0f), sf::Vector2f(300.0f, 350.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(230.0f, 50.0f), sf::Vector2f(780.0f, 200.0f));
-    construtorObstaculos("Espinhos", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(780.0f, 165.0f));
-    construtorObstaculos("Espinhos", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(810.0f, 165.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(230.0f, 50.0f), sf::Vector2f(780.0f, 600.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(230.0f, 50.0f), sf::Vector2f(1000.0f, 600.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(1300.0f, 150.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(1500.0f, 150.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(0.0f, 750.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(300.0f, 600.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(500.0f, 500.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(300.0f, 350.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(780.0f, 200.0f));
+    construtorObstaculos("Espinhos", sf::Vector2f(780.0f, 165.0f));
+    construtorObstaculos("Espinhos", sf::Vector2f(810.0f, 165.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(780.0f, 600.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(1000.0f, 600.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(1300.0f, 150.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(1500.0f, 150.0f));
 
-    construtorObstaculos("Espinhos", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(1450.0f, 120.0f));
-    construtorObstaculos("Espinhos", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(1500.0f, 120.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(1700.0f, 700.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(2080.0f, 600.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(2260.0f, 500.0f));
+    construtorObstaculos("Espinhos", sf::Vector2f(1450.0f, 120.0f));
+    construtorObstaculos("Espinhos", sf::Vector2f(1500.0f, 120.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(1700.0f, 700.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(2080.0f, 600.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(2260.0f, 500.0f));
 
-    construtorObstaculos("Plataforma", sf::Vector2f(700.0f, 50.0f), sf::Vector2f(3000.0f, 600.0f)); // soldado
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(2805.0f, 560.0f));
-    construtorObstaculos("Plataforma", sf::Vector2f(200.0f, 50.0f), sf::Vector2f(3695.0f, 560.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(3000.0f, 600.0f), sf::Vector2f(700.0f, 50.0f)); // soldado
+   // construtorObstaculos("Plataforma", sf::Vector2f(3000.0f, 600.0f)); // soldado
+    construtorObstaculos("Plataforma", sf::Vector2f(2805.0f, 560.0f));
+    construtorObstaculos("Plataforma", sf::Vector2f(3695.0f, 560.0f));
 
 
     // Criacao de obstaculos aleatorios
@@ -149,9 +150,9 @@ void Fase1::criarObstaculos() {
         int x = 1000 + (rand() % (2500 - 1000 + 1));
         int y = 0 + (rand() % (750 - 0 + 1));
 
-        construtorObstaculos("Caixa", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(x+100, 0.0f));
-        construtorObstaculos("Plataforma", sf::Vector2f(230.0f, 50.0f), sf::Vector2f(x-20, y));
-        construtorObstaculos("Espinhos", sf::Vector2f(50.0f, 50.0f), sf::Vector2f(x, y-40));
+        construtorObstaculos("Caixa", sf::Vector2f(x+100, 0.0f));
+        construtorObstaculos("Plataforma", sf::Vector2f(x-20, y));
+        construtorObstaculos("Espinhos", sf::Vector2f(x, y-40));
 
     }
 }
--- The Invasion/Fases/Fase1.h
+++ The Invasion/Fases/Fase1.h
@@ -11,7 +11,7 @@ namespace Fases {
 		~Fase1();
 		void executar();
 		void esvaziar();
-		void construtorObstaculos(const std::string& tipo, const sf::Vector2f& tam, const sf::Vector2f& pos);
+		void construtorObstaculos(const std::string& tipo, const sf::Vector2f& pos, sf::Vector2f tam = sf::Vector2f(200.0f, 50.0f));
 		void construtorPersonagens(const std::string& tipo, Entidades::Personagens::Jogador* pJogador, const sf::Vector2f& pos);
 		void criarPersonagens();
 		void criarObstaculos();
