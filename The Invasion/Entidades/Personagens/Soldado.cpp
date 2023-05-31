36058222ddf4b737b75d990b998151670926849b
Rafael Bergo <rafaelbergo503@gmail.com>
2023-05-31 15:49:28 -0300

    corrigido entidades no diagrama uml e adicionado atributo dano ao soldado

--- The Invasion/Entidades/Personagens/Soldado.cpp
+++ The Invasion/Entidades/Personagens/Soldado.cpp
@@ -8,6 +8,7 @@ Soldado::Soldado(sf::Vector2f pos, sf::Vector2f tam_corpo) : Inimigo(tam_corpo) 
     corpo.setPosition(pos);
     textura = pGrafico->carregarTextura(SOLDADO);
     corpo.setTexture(&textura);
+    
 }
 
 Soldado::~Soldado() {
--- The Invasion/Entidades/Personagens/Soldado.h
+++ The Invasion/Entidades/Personagens/Soldado.h
@@ -7,6 +7,8 @@ namespace Entidades {
     namespace Personagens {
 
         class Soldado : public Inimigo {
+        private:
+            int dano;
         public:
             Soldado(sf::Vector2f pos, sf::Vector2f tam_corpo = sf::Vector2f(80.0f, 80.0f));
             ~Soldado();
